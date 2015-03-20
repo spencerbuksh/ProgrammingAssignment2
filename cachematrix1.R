fb084eeaecb6ebc92f4f65055326702133b8ce24
Spencer Buksh <spencerbuksh@gmail.com>
2015-03-20 22:17:30 +0000

    Rename cachematrix.R to cachematrix1.R

---
+++ cachematrix1.R
@@ -0,0 +1,38 @@
+## These two functions allow a matrix x to be stored in the parent environment
+## and its inverse, m, to also be stored in the parent environment.
+## Then these stored matrices can be recalled rather than recalculated, hence saving processing time.
+
+## makeCacheMatrix starts with a matrix x, stores it is the parent enviroment, 
+## calculates its inverse and stores that in the parent environment.
+
+makeCacheMatrix <- function(x = matrix()) {
+m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+}
+
+
+## cacheSolve checks whether the inverse of x exists and if so retrieves the cached data
+## Else it calculates the inverse.
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+}
+Enter file contents here
